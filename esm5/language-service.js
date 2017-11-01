1ad561780eebc8daafe4910a49b4434e080fac8e
George Kalpakas <kalpakas.g@gmail.com>
2017-11-01 10:06:12 -0400

    bb807501c feat(elements): implement `registerAsCustomElements()`

--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.7-170f5c0c5
+ * @license Angular v5.0.0-rc.8-bb807501c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2596,7 +2596,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-rc.7-170f5c0c5');
+var VERSION = new Version('5.0.0-rc.8-bb807501c');
 
 /**
  * @license
